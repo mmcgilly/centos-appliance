dc692f4ce11786db1ef5c9f6cb6c38bd9d6d5c7b
Mark McGilly <mark@mmcgonline.info>
2020-09-12 15:44:17 +0100

    Change iso_checksum for Packer 1.6

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ rm -f output-vmware-iso/*.ova
 if [[ "$1" -gt "-1" ]] && [[ $1 == "dev" ]]; then
     echo "Applying packer build to centos-dev.json ..."
     date
-    /usr/local/bin/packer build -var-file=centos-builder.json -var-file=centos-version.json centos-dev.json
+    packer build -var-file=centos-builder.json -var-file=centos-version.json centos-dev.json
     date
 else
     echo "Applying packer build to centos.json ..."
     date
-    /usr/local/bin/packer156 build -var-file=centos-builder.json -var-file=centos-version.json centos.json
+    packer build -var-file=centos-builder.json -var-file=centos-version.json centos.json
     date
 fi
